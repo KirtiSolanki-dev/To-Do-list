9467265575d9d76c8d0e247d359b0054cfaf4fe6
Kirti Solanki <allmywork360@gmail.com>
2026-07-18 23:59:31 +0530

    js

---
+++ script.js
@@ -0,0 +1,14 @@
+const taskInput = document.getElementById("task-input");
+const addTaskBtn = document.getElementById("add-task-btn");
+const taskContainer = document.getElementById("task-container");
+
+addTaskBtn.addEventListener("click" , () => {
+    if (taskInput.value  === "" ){
+        return ;
+    }
+    
+    const taskText = taskInput.value;
+    const task = document.element("div");
+
+    
+});
