228079b82bd127fad532a25d30cb3f84df30b7de
Kirti Solanki <allmywork360@gmail.com>
2026-07-20 14:37:30 +0530

    "Added local storage and delete persistence"

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const taskInput = document.getElementById("task-input");
 const addTaskBtn = document.getElementById("add-task-btn");
 const taskContainer = document.getElementById("task-container");
+const totalTasks = document.getElementById("total-tasks");
+const pendingTasks = document.getElementById("pending-tasks");
+const completedTasks = document.getElementById("completed-tasks");
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+console.log(tasks);
 
-addTaskBtn.addEventListener("click", () => {
-    if (taskInput.value === "") {
-        return;
-    }
-
-    const taskText = taskInput.value;
-
+function createTask(taskText) {
     const task = document.createElement("div");
     task.classList.add("task");
 
@@ -26,15 +25,17 @@ addTaskBtn.addEventListener("click", () => {
 
     checkbox.addEventListener("change", () => {
 
-    if (checkbox.checked) {
-        taskSpan.style.textDecoration = "line-through";
-    } else {
-        taskSpan.style.textDecoration = "none";
-    }
+        if (checkbox.checked) {
+            taskSpan.style.textDecoration = "line-through";
+        } else {
+            taskSpan.style.textDecoration = "none";
+        }
+        updateCounters()
 
-});
+    });
 
     task.append(taskContent);
+
 
     const taskActions = document.createElement("div");
     taskActions.classList.add("task-actions");
@@ -53,32 +54,80 @@ addTaskBtn.addEventListener("click", () => {
 
     task.append(taskActions);
 
-    taskInput.value = "";
+    taskContainer.append(task);
+    updateCounters();
 
-     taskContainer.append(task);
+    deleteBtn.addEventListener("click", () => {
+        tasks = tasks.filter((t) => t !== taskText);
 
-     deleteBtn.addEventListener("click", ()=>{
-     task.remove();
+        localStorage.setItem(
+            "tasks",
+            JSON.stringify(tasks)
+        );
 
+        task.remove();
+        updateCounters()
+
+    });
+
+    editBtn.addEventListener("click", () => {
+
+        if (editBtn.textContent === "Edit") {
+
+            taskSpan.contentEditable = true;
+            taskSpan.focus();
+            checkbox.disabled = true;
+            editBtn.textContent = "Save";
+
+        } else {
+
+            taskSpan.contentEditable = false;
+            editBtn.textContent = "Edit";
+            checkbox.disabled = false;
+        }
+
+
+    });
+
+}
+
+
+function updateCounters() {
+    const total = document.querySelectorAll(".task").length
+    const completed = document.querySelectorAll(".task input[type = 'checkbox']:checked").length
+
+    const pending = total - completed
+
+    totalTasks.textContent = total;
+    pendingTasks.textContent = pending;
+    completedTasks.textContent = completed;
+}
+
+tasks.forEach((task) => {
+    createTask(task);
 });
-  
-editBtn.addEventListener("click", () => {
 
-    if (editBtn.textContent === "Edit") {
 
-        taskSpan.contentEditable = true;
-        taskSpan.focus();
-        checkbox.disabled= true;
-        editBtn.textContent = "Save";
 
-    } else {
-
-        taskSpan.contentEditable = false;
-        editBtn.textContent = "Edit";
-         checkbox.disabled= false;
+addTaskBtn.addEventListener("click", () => {
+    if (taskInput.value === "") {
+        return;
     }
 
-});
+    const taskText = taskInput.value;
+
+    tasks.push(taskText);
+
+    localStorage.setItem(
+        "tasks",
+        JSON.stringify(tasks)
+    );
+
+    createTask(taskText);
+
+    taskInput.value = "";
+    updateCounters();
+
 
 });
 
